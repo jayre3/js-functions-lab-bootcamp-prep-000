3c13becee64e2dff76a274ccb68d60072f4d3573
Jay Singh <jeewandeep.singh@gmail.com>
2018-07-05 01:57:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function happyHolidays() {
   return "Happy holidays!"
-}
+};
 
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
-}
+};
 
 function happyHolidaysTo(holiday, name) {
   return 'Happy ${holiday}, ${name}'
-}
+};
